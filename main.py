5c5457d7f81cf38c7c3babf0ae2e5f17ccad8966
Ike <dylanraspino264@gmail.com>
2024-05-02 22:14:03 +0000

    use lines instead of buttons

--- main.py
+++ main.py
@@ -15,35 +15,35 @@ def main() -> None:
 
     # Button class
 
-    class Button:
+    # class Button:
 
-        def __init__(self, x: int, y: int, width: int, height: int, text: Optional[str]=None, action=None) -> None:
-            self.rect = pygame.Rect(x, y, width, height)
-            self.color = (0, 0, 0)
-            self.text = text
-            self.action = action
+    #     def __init__(self, x: int, y: int, width: int, height: int, text: Optional[str]=None, action=None) -> None:
+    #         self.rect = pygame.Rect(x, y, width, height)
+    #         self.color = (0, 0, 0)
+    #         self.text = text
+    #         self.action = action
 
-        def draw(self, screen) -> None:
-            pygame.draw.rect(screen, self.color, self.rect)
-            # font = pygame.font.SysFont(None, 20)
-            # text = font.render(self.text, True, WHITE)
-            # text_rect = text.get_rect(center=self.rect.center)
-            # screen.blit(text, text_rect)
+    #     def draw(self, screen) -> None:
+    #         pygame.draw.rect(screen, self.color, self.rect)
+    #         # font = pygame.font.SysFont(None, 20)
+    #         # text = font.render(self.text, True, WHITE)
+    #         # text_rect = text.get_rect(center=self.rect.center)
+    #         # screen.blit(text, text_rect)
 
-        def is_clicked(self, pos) -> bool:
-            return self.rect.collidepoint(pos)
+    #     def is_clicked(self, pos) -> bool:
+    #         return self.rect.collidepoint(pos)
         
-        # create buttons
+    #     # create buttons
 
-    top_left = Button(50, 50, 70, 50)
-    top_middle = Button(150, 50, 70, 50)
-    top_right = Button(250, 50, 70, 50)
-    middle_left = Button(50, 120, 70, 50)
-    middle = Button(150, 120, 70, 50)
-    middle_right = Button(250, 120, 70, 50)
-    bottom_left = Button(50, 190, 70, 50)
-    bottom_middle = Button(150, 190, 70, 50)
-    bottom_right = Button(250, 190, 70, 50)
+    # top_left = Button(50, 50, 70, 50)
+    # top_middle = Button(150, 50, 70, 50)
+    # top_right = Button(250, 50, 70, 50)
+    # middle_left = Button(50, 120, 70, 50)
+    # middle = Button(150, 120, 70, 50)
+    # middle_right = Button(250, 120, 70, 50)
+    # bottom_left = Button(50, 190, 70, 50)
+    # bottom_middle = Button(150, 190, 70, 50)
+    # bottom_right = Button(250, 190, 70, 50)
 
     while running:
 
@@ -62,18 +62,24 @@ def main() -> None:
             #         print(f'button was pressed at {mouse_pos}')
 
 
-        screen.fill("yellow")
+        screen.fill("black")
         
         # create buttons
-        top_left.draw(screen)
-        top_middle.draw(screen)
-        top_right.draw(screen)
-        middle_left.draw(screen)
-        middle.draw(screen)
-        middle_right.draw(screen)
-        bottom_left.draw(screen)
-        bottom_middle.draw(screen)
-        bottom_right.draw(screen)
+        # top_left.draw(screen)
+        # top_middle.draw(screen)
+        # top_right.draw(screen)
+        # middle_left.draw(screen)
+        # middle.draw(screen)
+        # middle_right.draw(screen)
+        # bottom_left.draw(screen)
+        # bottom_middle.draw(screen)
+        # bottom_right.draw(screen)
+
+        pygame.draw.line(screen, "yellow", (150, 0), (150, 300))
+        pygame.draw.line(screen, "yellow", (250, 0), (250, 300))
+        pygame.draw.line(screen, "yellow", (0, 100), (400, 100))
+        pygame.draw.line(screen, "yellow", (0, 200), (400, 200))
+
 
 
 
